9643bd7ab6aa5ff23ded6cf07c885f3782539155
Melvin Moukam <moukammelvin@gmail.com>
2023-02-15 00:25:09 +0100

    Héritage_Polymorphysme

---
+++ src/les_classes/Animal.java
@@ -0,0 +1,19 @@
+package les_classes;
+
+public class Animal {
+	  
+  public  void déplacer() {
+	  
+		System.out.println("je me déplace");
+
+	}
+  
+  public static void main(String[] args) {
+		
+		Animal a1 = new Chien();
+		
+		a1.déplacer();
+		
+		
+	}
+}
---
+++ src/les_classes/Chien.java
@@ -0,0 +1,14 @@
+package les_classes;
+
+public class Chien extends Animal {
+	
+	@Override
+	public void déplacer() {
+		
+	   
+	   System.out.println("je marche");
+		
+	}
+	
+
+}
